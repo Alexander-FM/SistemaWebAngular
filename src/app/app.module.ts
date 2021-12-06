da9bf323bc68ff2d7c6da94eafbedfe637b85596
Alexander Fuentes Medina <lfuenmed@emeal.nttdata.com>
2021-12-06 13:33:17 -0500

    Sistema web con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './vistas/plus/navbar/navbar.component';
+import { SidebarComponent } from './vistas/plus/sidebar/sidebar.component';
+import { FooterComponent } from './vistas/plus/footer/footer.component';
+import { CategoriasComponent } from './vistas/categorias/categorias.component';
+import { MarcasComponent } from './vistas/marcas/marcas.component';
+import { ProductosComponent } from './vistas/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    CategoriasComponent,
+    MarcasComponent,
+    ProductosComponent,
   ],
   imports: [
     BrowserModule,
